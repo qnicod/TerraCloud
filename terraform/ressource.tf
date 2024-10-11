5a743fb9caf5cfe53d5c2bfd4d31b566de4edc3f
shuto <quentin.nicod@epitech.eu>
2024-10-11 14:37:02 +0200

    [FIX] replace storage_type Premium to Standard

--- terraform/ressource.tf
+++ terraform/ressource.tf
@@ -1,5 +1,5 @@
 resource "azurerm_dev_test_linux_virtual_machine" "example" {
-  name                   = "terra-cloud-application-vm-example"
+  name                   = "terra-cloud-application-vm"
   resource_group_name    = data.azurerm_resource_group.existing_rg.name
   lab_name               = data.azurerm_dev_test_lab.existing_lab.name
   location               = data.azurerm_resource_group.existing_rg.location
@@ -8,7 +8,7 @@ resource "azurerm_dev_test_linux_virtual_machine" "example" {
   password               = "Password1234!"
   lab_virtual_network_id = data.azurerm_dev_test_virtual_network.existing_vnet.id
   lab_subnet_name        = data.azurerm_dev_test_virtual_network.existing_vnet.allowed_subnets[0].lab_subnet_name
-  storage_type           = "Premium"
+  storage_type           = "Standard"
   notes                  = "Application VM for the Terraform Cloud example."
 
   gallery_image_reference {
