14d72557ca463aa801a0caa1082f628fac860568
shuto <quentin.nicod@epitech.eu>
2024-10-11 14:11:56 +0200

    [FIX] subnet output

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,9 +15,8 @@ data "azurerm_dev_test_virtual_network" "existing_vnet" {
   name                = "t-clo-901-rns-1"
   lab_name            = data.azurerm_dev_test_lab.existing_lab.name
   resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
 
-  subnet {
-    use_public_ip_address           = "Allow"
-    use_in_virtual_machine_creation = "Allow"
-  }
+output "lab_subnet_name" {
+  value = data.azurerm_dev_test_virtual_network.existing_vnet.allowed_subnets[0].lab_subnet_name
 }
