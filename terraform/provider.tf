8534ffbfd9f12f5e622668912e552c109cd11e64
shuto <quentin.nicod@epitech.eu>
2024-10-11 12:23:06 +0200

    [REMOVE] subnet parameters

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,9 +15,4 @@ data "azurerm_dev_test_virtual_network" "existing_vnet" {
   name                = "t-clo-901-rns-1"
   lab_name            = data.azurerm_dev_test_lab.existing_lab.name
   resource_group_name = data.azurerm_resource_group.existing_rg.name
-
-  subnet {
-    use_public_ip_address           = "Allow"
-    use_in_virtual_machine_creation = "Allow"
-  }
 }
