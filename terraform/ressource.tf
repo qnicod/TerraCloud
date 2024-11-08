8bd4516b47c82a893d96802171fc0451825abcfb
shuto <quentin.nicod@epitech.eu>
2024-11-08 10:08:05 +0100

    [ADD] ssh key for app vm

--- terraform/ressource.tf
+++ terraform/ressource.tf
@@ -1,15 +1,17 @@
 resource "azurerm_dev_test_linux_virtual_machine" "example" {
-  name                   = "terra-cloud-application-vm"
-  resource_group_name    = data.azurerm_resource_group.existing_rg.name
-  lab_name               = data.azurerm_dev_test_lab.existing_lab.name
-  location               = data.azurerm_resource_group.existing_rg.location
-  size                   = "Standard_A4_v2"
-  username               = "maintainer"
-  password               = var.PASSWORD_APP_VM
-  lab_virtual_network_id = data.azurerm_dev_test_virtual_network.existing_vnet.id
-  lab_subnet_name        = data.azurerm_dev_test_virtual_network.existing_vnet.allowed_subnets[0].lab_subnet_name
-  storage_type           = "Standard"
-  notes                  = "Application VM for the Terraform Cloud example."
+  name                            = "terra-cloud-application-vm"
+  resource_group_name             = data.azurerm_resource_group.existing_rg.name
+  lab_name                        = data.azurerm_dev_test_lab.existing_lab.name
+  location                        = data.azurerm_resource_group.existing_rg.location
+  size                            = "Standard_A4_v2"
+  lab_virtual_network_id          = data.azurerm_dev_test_virtual_network.existing_vnet.id
+  lab_subnet_name                 = data.azurerm_dev_test_virtual_network.existing_vnet.allowed_subnets[0].lab_subnet_name
+  storage_type                    = "Standard"
+  notes                  	      = "Application VM for the Terraform Cloud example."
+  disable_password_authentication = true
+  username                        = "maintener"
+  ssh_key                         = file("~/.ssh/id_rsa.pub")
+
 
   gallery_image_reference {
     publisher = "Canonical"
