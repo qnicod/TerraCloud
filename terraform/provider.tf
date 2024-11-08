bf3273c8a69fa4525c1f6bb3090daab8282324db
shuto <quentin.nicod@epitech.eu>
2024-11-08 10:09:25 +0100

    [REMOVE] output content

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,7 +16,3 @@ data "azurerm_dev_test_virtual_network" "existing_vnet" {
   lab_name            = data.azurerm_dev_test_lab.existing_lab.name
   resource_group_name = data.azurerm_resource_group.existing_rg.name
 }
-
-output "lab_subnet_name" {
-  value = data.azurerm_dev_test_virtual_network.existing_vnet.allowed_subnets[0].lab_subnet_name
-}
