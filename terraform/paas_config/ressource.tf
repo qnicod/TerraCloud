4990fb62df742b4f964bf2311ef4f78e0ec0da16
shuto <quentin.nicod@epitech.eu>
2025-01-10 11:19:31 +0100

    [FIX] terraform ressource paas

--- terraform/paas_config/ressource.tf
+++ terraform/paas_config/ressource.tf
@@ -10,7 +10,7 @@ resource "azurerm_linux_web_app" "app" {
   name                = "terra-cloud-paas-via-semaphore"
   location            = data.azurerm_resource_group.existing_rg.location
   resource_group_name = data.azurerm_resource_group.existing_rg.name
-  service_plan_id     = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {}
 
